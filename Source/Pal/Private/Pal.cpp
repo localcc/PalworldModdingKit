5a4117fcf45164c737d5ef7845aef35a8a529796
localcc <work@localcc.cc>
2024-01-26 13:48:32 +0100

    Fix compilation

--- Source/Pal/Private/Pal.cpp
+++
@@ -1,4 +0,0 @@
-#include "Pal.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pal, "Pal" );
--- Source/Pal/Public/Pal.h
+++
@@ -1,2 +0,0 @@
-#include "Engine.h"
-#include "PalClasses.h"
