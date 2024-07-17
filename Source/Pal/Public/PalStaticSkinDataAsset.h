f1661df39a002d60427e9066f1749c1ede930bbb
Okaetsu <111317036+Okaetsu@users.noreply.github.com>
2024-07-17 12:46:56 +0300

    Make PalStaticSkinDataAsset editable (#35)

--- Source/Pal/Public/PalStaticSkinDataAsset.h
+++ Source/Pal/Public/PalStaticSkinDataAsset.h
@@ -9,7 +9,7 @@ UCLASS(Blueprintable)
 class PAL_API UPalStaticSkinDataAsset : public UDataAsset {
     GENERATED_BODY()
 public:
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Instanced, meta=(AllowPrivateAccess=true))
     TMap<FName, UPalSkinDataBase*> StaticSkinMap;
     
     UPalStaticSkinDataAsset();
